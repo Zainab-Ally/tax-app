65acd26d9a3801df175266a03ed33d6972db5ee8
Zainab-Ally <emmy24ally@yahoo.com>
2017-04-19 18:45:35 +0300

    initiat comint

---
+++ README.md
@@ -0,0 +1,24 @@
+# Ionic 2 Leaflet Map Example
+
+Ionic 2 Example App with Leaflet using OpenStreetMap Layers.
+
+### how to build to this step
+
+	# ionic start
+	ionic start ionic2-map-leaflet tabs --v2
+
+	# --- commit ---
+
+	# add leaflet lib
+	mkdir src/assets/images/
+	cp node_modules/leaflet/dist/leaflet.css src/assets
+	cp node_modules/leaflet/dist/images/* src/assets/images
+
+	# add page to show map
+	ionic g page map
+	# update 'src/pages/map/*'
+	# update 'src/pages/tabs/*'
+	# update declarations, entryComponents in 'app.module.ts'
+	# include `external` leaflet.css from assets in 'index.html'
+
+	# --- commit ---
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { MapPage } from '../pages/map/map';
+import {AboutPage} from "../pages/about/about";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    MapPage,AboutPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    MapPage,AboutPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
