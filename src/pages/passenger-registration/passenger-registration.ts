adcc67bdf494328a7bb36c99de98faa2d1bbde87
Zainab-Ally <emmy24ally@yahoo.com>
2017-05-27 12:24:29 +0300

    some changes in passenger's form

--- src/pages/passenger-registration/passenger-registration.ts
+++ src/pages/passenger-registration/passenger-registration.ts
@@ -1,60 +1,72 @@
-import { Component } from '@angular/core';
-import { NavController, ToastController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, ToastController} from 'ionic-angular';
 import {HttpClient} from "../../providers/http-client";
 
 /*
-  Generated class for the PassengerRegistration page.
+ Generated class for the PassengerRegistration page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
 @Component({
   selector: 'page-passenger-registration',
   templateUrl: 'passenger-registration.html',
-  providers : [HttpClient]
+  providers: [HttpClient]
 })
 export class PassengerRegistrationPage {
 
-  public mandatoryFields : any;
-  public data : any= {};
+  public mandatoryFields: any;
+  public data: any = {};
   public loadingData: boolean;
-  public loadingMessages : any = [];
+  public loadingMessages: any = [];
 
-  public selectedCategories : any;
+  public selectedCategories: any;
 
-  constructor(public navCtrl: NavController, private toastCtrl: ToastController, private httpClient:HttpClient) {
-  this.loadingData = false;
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController, private httpClient: HttpClient) {
+    this.loadingData = false;
     this.mandatoryFields = [
-      {name : "fname",key : "fname"},
-      {name : "mname",key : "mname"},
-      {name : "lname",key : "lname"},
-      {name : "pnumber",key : "pnumber"},
-      {name : "address",key : "address"},
-      {name : "email",key : "email"}
+      {name: "PID", key: "PID"},
+      {name: "pfname", key: "pfname"},
+      {name: "pmname", key: "pmname"},
+      {name: "plname", key: "plname"},
+      {name: "pnumber", key: "pnumber"},
+      {name: "apaltphone", key: "paltphone"},
+      {name: "pemail", key: "pemail"},
+      {name: "pphysical_address", key: "pphysical_address"}
     ];
 
   }
 
-  sumitForm()
-{
+  isAllMandatoryFieldsEntered(){
+
+  }
+  setLoadingMessages(msg){
+
+  }
+
+  setToasterMessage(msg){
+
+  }
+
+  submitForm() {
     console.log("Form is ready for submission");
 
-    if(this.isAllMandatoryFieldsEntered() && this.selectedCategories.length > 0){
+    if (this.isAllMandatoryFieldsEntered() && this.selectedCategories.length > 0) {
       let url = "/passenger_registration";
       this.loadingData = true;
       this.loadingMessages = [];
       //this.data.uni_id = this.selectedUniversity
       this.setLoadingMessages('Register account');
-      this.httpClient.post(url,this.data).subscribe((response)=>{
+      this.httpClient.post(url, this.data).subscribe((response) => {
         response = response.json();
-       // this.setUserData(response);
-      },error=>{
+        // this.setUserData(response);
+      }, error => {
         this.loadingData = false;
         this.setToasterMessage("Fail to register account");
       });
+    }
+
+
   }
-
-
-}
 
 }
